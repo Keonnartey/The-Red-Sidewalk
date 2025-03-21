5277c82ba455f5f6f45d765ee2ebedc7ba613a9c
AaryaDesai1 <aad64@duke.edu>
2025-03-21 15:15:26 -0400

    adding agg schema

---
+++ db/agg_schema.sql
@@ -0,0 +1,33 @@
+-- Create Schema
+CREATE SCHEMA IF NOT EXISTS agg;
+
+------------------------------------------------------------
+-- Sightings_Ratings Table
+------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS agg.sightings_ratings (
+    sighting_id INT PRIMARY KEY,
+    avg_rating FLOAT DEFAULT 0,
+    rating_count INT DEFAULT 0,
+    FOREIGN KEY (sighting_id) REFERENCES info.sightings_preview(sighting_id) ON DELETE CASCADE
+);
+
+------------------------------------------------------------
+-- Click_Data Table
+------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS agg.click_data (
+    sighting_id INT PRIMARY KEY,
+    total_clicks INT DEFAULT 0,
+    total_comments INT DEFAULT 0,
+    FOREIGN KEY (sighting_id) REFERENCES info.sightings_preview(sighting_id) ON DELETE CASCADE
+);
+
+------------------------------------------------------------
+-- Creatures Table
+------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS agg.creatures (
+    creature_id SERIAL PRIMARY KEY,
+    creature_name VARCHAR(255) NOT NULL,
+    avg_rating FLOAT DEFAULT 0,
+    avg_height FLOAT DEFAULT 0,
+    avg_weight FLOAT DEFAULT 0
+);
