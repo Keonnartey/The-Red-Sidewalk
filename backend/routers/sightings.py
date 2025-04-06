37ae9ebe6bffd6bc9f1eebfc39edff8f410d1293
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2025-04-06 16:44:28 -0700

    Update sightings.py

--- backend/routers/sightings.py
+++ backend/routers/sightings.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from database import get_db
@@ -9,15 +8,4 @@ router = APIRouter()
 
 @router.get("/")
 def get_all_sightings(db: Session = Depends(get_db)):
-=======
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from database import get_db
-from services import sightings as sightings_service
-
-router = APIRouter()
-
-@router.get("/")
-def get_all_sightings(db: Session = Depends(get_db)):
->>>>>>> 3b6ba7de1c6fef58cb36dfd8b95993ba6dbcedfc
     return sightings_service.get_all_sightings(db)
