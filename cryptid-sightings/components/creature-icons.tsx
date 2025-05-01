98926572e4b6734681f27c6906dbfa61403c1697
AaryaDesai1 <aad64@duke.edu>
2025-05-01 17:37:59 -0400

    Adding alien icon new

--- cryptid-sightings/components/creature-icons.tsx
+++ cryptid-sightings/components/creature-icons.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import { motion } from "framer-motion"
 // new imports
 import { GiFootprint, GiVampireCape } from "react-icons/gi"
+import { RiAliensFill } from "react-icons/ri";
 
 
 interface CreatureIconProps {
@@ -104,15 +105,8 @@ export function DragonIcon({ color = "Blue-ish", size = 24 }) {
   )
 }
 
-export function AlienIcon({ color = "Blue-ish", size = 24 }) {
-  return (
-    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z"
-        fill={color}
-      />
-    </svg>
-  )
+export function AlienIcon({ color = "#03ff00", size = 24 }) {
+  return <RiAliensFill color={color} size={size} />
 }
 
 export function SunIcon({ color = "black", size = 24 }) {
